80a25b3042ca570904d9103a37308d7936f5bf1a
Matheus de Assis Silva <matheusassissilva20@gmail.com>
2023-11-15 22:07:08 -0300

    test: add value validation test

--- test/CreateTransaction.test.ts
+++ test/CreateTransaction.test.ts
@@ -1,4 +1,5 @@
 import { CreateTransaction } from "../src/CreateTransaction";
+import { InvalidValueTransactionError } from "../src/InvalidValueTransactionError";
 
 test("Criar uma transação", async function() {
     const sut = new CreateTransaction();
@@ -11,4 +12,15 @@ test("Criar uma transação", async function() {
     const result = await sut.execute(input)
 
     expect(result.message).toBe("transaction successfully");
+})
+
+test("Não criar uma transação caso o valor seja menor ou igual a 0 (zero)", function() {
+    const sut = new CreateTransaction();
+
+    const input = {
+        value: -100.0,
+        payee: "Maria"
+    }
+
+    expect(async () => await sut.execute(input)).rejects.toThrow(new InvalidValueTransactionError("Invalid value for transaction"))
 })
